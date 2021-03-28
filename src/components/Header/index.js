345f516043467013a671dc34fbad884b6cc325a5
andrzejgal <andrzejmg@op.pl>
2021-03-28 12:37:12 +0200

    I commented class component out

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,12 @@
-import React from "react";
 
-// const Header = (props) => {
-//     return (
-//         <div>
-//             <h1>To jest kurs react-funkcja,witam Cię {props.name}</h1>
-//         </div>
-//     )
-// }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>To jest kurs react-klasa,witam Cię {this.props.name}</h1>
-            </div>
-        )    
-    }
+const Header = (props) => {
+    return (
+        <div>
+            <h1>To jest kurs react-funkcja,witam Cię {props.name}</h1>
+        </div>
+    )
 }
+
 
 export default Header;
